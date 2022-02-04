5e39696d6397fab6cea272162d1d5e0beda3abf2
vv_tomilin <backatom1@gmail.com>
2022-02-04 17:49:30 +0500

    2_lesson <travel-app> (Футер и навигация)

---
+++ travel_app/app/components/common/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const navItems = [
+  {
+    link: '/',
+    icon: 'home'
+  },
+  {
+    link: '/explore',
+    icon: 'explore'
+  },
+  {
+    link: '/place',
+    icon: 'place'
+  },
+  {
+    link: '/profile',
+    icon: 'person_outline'
+  },
+];
+
+const Footer = () => {
+
+  const { push, pathname } = useRouter();
+
+  return (
+    <footer className={styles.footer}>
+      <nav>
+        {navItems.map((item, index) => (
+          <button
+            key={index}
+            onClick={() => push(item.link)}
+            className={pathname === item.link ? styles.active : ''}
+          >
+            <span className='material-icons-outlined'>{item.icon}</span>
+          </button>
+        ))}
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
